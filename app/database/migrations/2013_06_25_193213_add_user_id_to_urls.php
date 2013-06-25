faa327fcd065e51a21d6e376dc6127c978ebc8b8
KamalN7 <kamal@kamalnasser.net>
2013-06-25 22:38:56 +0300

    gah PSR.

--- app/database/migrations/2013_06_25_193213_add_user_id_to_urls.php
+++ app/database/migrations/2013_06_25_193213_add_user_id_to_urls.php
@@ -3,36 +3,41 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddUserIdToUrls extends Migration {
+class AddUserIdToUrls extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('urls', function(Blueprint $table)
-		{
-			$table->integer('user_id')->unsigned()->after('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'urls',
+            function (Blueprint $table) {
+                $table->integer('user_id')->unsigned()->after('id');
 
-            $table->foreign('user_id')->references('id')->on('users');
-        });
-	}
+                $table->foreign('user_id')->references('id')->on('users');
+            }
+        );
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('urls', function(Blueprint $table)
-		{
-            $table->dropColumn('user_id');
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'urls',
+            function (Blueprint $table) {
+                $table->dropColumn('user_id');
 
-            $table->dropForeign('urls_user_id_foreign');
-        });
-	}
+                $table->dropForeign('urls_user_id_foreign');
+            }
+        );
+    }
 
 }
